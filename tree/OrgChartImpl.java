e1bae0c1fad4ae8ca85a6f8b5d905a8592c76cbd
Nicole <nicole.reagan@student.fairfield.edu>
2024-03-20 15:19:51 -0400

    This is your comment

--- tree/OrgChartImpl.java
+++ tree/OrgChartImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
  
 public class OrgChartImpl implements OrgChart{
 
+	//hello
 	//Employee is your generic 'E'..
 	private List<GenericTreeNode<Employee>> nodes = new ArrayList<>();
 	
